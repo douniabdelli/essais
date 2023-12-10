50d941458e60e8b5abd9290d7da7eb38f8115ad7
ab.bouzidi <abdeldjalil.bouzidi@etu.usthb.dz>
2023-12-10 12:11:24 +0100

    change 'ok' to 'Annuler'

--- lib/services/dio.dart
+++ lib/services/dio.dart
@@ -7,12 +7,12 @@ Dio dio() {
   dio.options.headers['accept'] = 'Application/Json';
 
   // ios
-  // dio.options.baseUrl = "http://localhost:8000/api";
+  // dio.options.baseUrl = "http://localhost:8000/api/mobile";
   // android
-  //dio.options.baseUrl = "http://10.0.2.2:8000/api";
+  //dio.options.baseUrl = "http://10.0.2.2:8000/api/mobile";
 
-  //dio.options.baseUrl = "http://192.168.15.96:8000/api";
-  dio.options.baseUrl = "http://192.168.108.2:96/api";
+  dio.options.baseUrl = "http://192.168.15.98:8000/api/mobile";
+  //dio.options.baseUrl = "http://192.168.108.2:96/api/mobile";
 
   return dio;
 }
